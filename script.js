add269a1835aa83f9274e8d780cb08f31de9cc63
Prof. Sandro Martins <profsandromartins@gmail.com>
2025-11-07 11:53:40 -0300

    Alteração
    Vamos marcar

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("Ola mundo");
 
 let churrasco = "sim";
 
-if ( churrasco == "sim") {
+if ( churrasco == "nao") {
     alert("vai ter churrasco!");
 } else {
     alert("Então vamos marcar o churrasco!");
